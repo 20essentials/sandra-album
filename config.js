ada6c9170f25bbe4b38102162c9f426f12bcecfc
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:44:54 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SANDRA';
+export let nameSongs = [
+	"MARIA MAGDALENA", /*1*/
+	"IN THE HEAT OF THE NIGHT", /*2*/
+	"SECRET LAND", /*3*/
+	"DON’T CRY", /*4*/
+	"YOU AND I", /*5*/
+	"STAY IN TOUCH", /*6*/
+	"LOVE STARTS WITH A SMILE", /*7*/
+	"SUN IN DISGUISE", /*8*/
+	"HEART OF WAX", /*9*/
+	"SUCH A SHAME", /*10*/
+	"HIROSHIMA", /*11*/
+	"MEDLEY: THE JOURNEY", /*12*/
+	"ANGELS IN MY HEAD", /*13*/
+	"HEAVEN CAN WAIT", /*14*/
+	"INNOCENT LOVE", /*15*/
+	"EVERLASTING LOVE", /*16*/
+	"LITTLE GIRL", /*17*/
+	"HI! HI! HI!", /*18*/
+	"LOREEN", /*19*/
+	"JUST LIKE DIAMONDS", /*20*/
+]
